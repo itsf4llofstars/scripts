461d43f34a38137de46ae336c9f20eb29a22f8c2
itsf4llofstars <irooted4hal@mailfence.com>
2022-09-13 02:05:02 -0500

    Add copy files to temp directory

--- bash/tar-dotfiles.sh
+++ bash/tar-dotfiles.sh
@@ -40,7 +40,8 @@ then
                 rm "$HOME"/"$tar_dirname"/"$tar_filename"
         else
                 #TODO: We could log is output
-                printf "File: $tar_filename does not exist\n"
+                # printf "File: $tar_filename does not exist\n"
+                echo
         fi
 else
         # TODO: We could log is output
@@ -48,15 +49,33 @@ else
         exit
 fi
 
+
 temp_dirname="temp_dots"
 
 # Check if temporary directory exists, delete any files if it does
 # create if if doesn't
 if [ -d "$HOME"/"$temp_dirname" ]
 then
-        rm "$HOME"/"$temp_dirname"/*
+        # TODO: chick if dir is empty or not
+        # rm  "$HOME"/"$temp_dirname"/*
+        echo
 else
         mkdir "$HOME"/"$temp_dirname"
 fi
 
+sleep 6
+
+# Copy the dot files.
+if [ -d "$HOME"/"$temp_dirname" ]
+then
+        cp "$HOME"/.bashrc "$HOME"/"$temp_dirname"/bashrc.bak
+        cp "$HOME"/.bash_aliases "$HOME"/"$temp_dirname"/bash_aliases.bak
+        cp "$HOME"/.vimrc "$HOME"/"$temp_dirname"/vimrc.bak
+        cp "$HOME"/.tmux.conf "$HOME"/"$temp_dirname"/tmux.conf.bak
+        cp "$HOME"/.gitconfig "$HOME"/"$temp_dirname"/gitconfig.bak
+        cp "$HOME"/.nanorc "$HOME"/"$temp_dirname"/nanorc.bak
+        # cp "$HOME"/. "$HOME"/"temp_dirname"/
+fi
+
 exit
+
