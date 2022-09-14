344f2837f178a4a3b23b745c28309203d742d6bc
itsf4llofstars <irooted4hal@mailfence.com>
2022-09-13 19:44:35 -0500

    Add help section and documentation

--- bash/tar-codeonly-dotfiles.sh
+++ bash/tar-codeonly-dotfiles.sh
@@ -1,21 +1,51 @@
 #!/usr/bin/env bash
 
+##: TITLE:       tar-dotfiles.sh
+##: DATE:        22/09/08 - YMD
+##: AUTHOR:      GitHub: itsf4llofstars
+##: VERSION:     0.1.0
+##: DESCRPTION:  Creates a tar archives of common dot files
+##: OPTIONS:     -h, --help,
+
+##: Display help text
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+        clear
+        cat <<EOF
+Adds common dotfiles (configuration files) to a tar file
+in an directory of the user choice. Good if used with crontab scheduler.
+
+If included see tar-dotfiles.md file for additional notes.
+
+Call:
+$ ./tar-dotfile.sh [options]
+
+options
+    -h, --help  This help text.
+EOF
+        exit
+fi
+
+## Constant filename and pathname variables
 ARCHIVE_PATH="$HOME/archives"
 ARCHVIE_FILE="dotfiles-backup.tar"
 ARCHIVE_PATHFILE="$HOME/archives/dotfiles-backup.tar"
 TEMP_PATH="$HOME/temp_dots"
 LOG_FILE="$HOME/logfiles/script_error.txt"
 
-if [ -d "$TEMP_PATH" ]; then
-        rm -r "$TEMP_PATH"
-        echo "$TEMP_PATH removed" >>"$LOG_FILE"
-fi
+## Checks if TEMP_PATH exists and deletes it if it does
+# if [ -d "$TEMP_PATH" ]; then
+#         rm -r "$TEMP_PATH"
+#         echo "$TEMP_PATH removed" >>"$LOG_FILE"
+# fi
 
+## If the TEMP_PATH does not exist it is created here
 if [ ! -d "$TEMP_PATH" ]; then
         mkdir "$TEMP_PATH"
         echo "$TEMP_PATH created" >>"$LOG_FILE"
 fi
 
+## Checks if TEMP_PATH exists and if so copies the dot files
+## to TEMP_PATH renaming theme as *.bak files
 if [ -d "$TEMP_PATH" ]; then
         cp "$HOME"/.bashrc "$TEMP_PATH"/bashrc.bak
         cp "$HOME"/.bash_aliases "$TEMP_PATH"/bash_aliases.bak
@@ -27,14 +57,22 @@ if [ -d "$TEMP_PATH" ]; then
         echo "Files Copied" >>"$LOG_FILE"
 fi
 
+## Checs if tar file exists in ARCHIVE_PATHFILE and deletes it if it doex
 if [ -f "$ARCHIVE_PATHFILE" ]; then
         rm "$ARCHIVE_PATHFILE"
         echo "$ARCHIVE_PATHFILE removed" >>"$LOG_FILE"
 fi
 
+## Changes working directory to TERMP_PATH and creates the tar file
+## in ARCHIVE_PATHFILE
 cd "$TEMP_PATH"
 tar -cf "$ARCHIVE_PATHFILE" .
+cd "$HOME"
 
+## Removes the TEMP_PATH if the tar file was created
 if [ -f "$ARCHIVE_PATHFILE" ]; then
-        echo "tar success" >>"$LOG_FILE"
+        rm -r "$TEMP_PATH"
 fi
+
+## Logs scritp run to a log file
+printf "$(date +'%FT%R:%S%::z')00 $HOME/bashscripts/tar-dotfiles.sh\n" >>"$HOME"/logfiles/cron.log 2>&1
