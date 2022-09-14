51ea25f9c27929cbece670ea738a7f752a5dcb49
itsf4llofstars <irooted4hal@mailfence.com>
2022-09-13 19:30:12 -0500

    Add working non-documented code

--- bash/tar-codeonly-dotfiles.sh
+++ bash/tar-codeonly-dotfiles.sh
@@ -4,35 +4,37 @@ ARCHIVE_PATH="$HOME/archives"
 ARCHVIE_FILE="dotfiles-backup.tar"
 ARCHIVE_PATHFILE="$HOME/archives/dotfiles-backup.tar"
 TEMP_PATH="$HOME/temp_dots"
+LOG_FILE="$HOME/logfiles/script_error.txt"
 
-if [ -d "$TEMP_PATH" ]
-then
+if [ -d "$TEMP_PATH" ]; then
         rm -r "$TEMP_PATH"
-        echo "temp_dots dir rmvd"
+        echo "$TEMP_PATH removed" >>"$LOG_FILE"
 fi
 
-if [ ! -d "$TEMP_PATH" ]
-then
+if [ ! -d "$TEMP_PATH" ]; then
         mkdir "$TEMP_PATH"
-        echo "temp_dots dir created"
+        echo "$TEMP_PATH created" >>"$LOG_FILE"
 fi
 
-if [ -d "$TEMP_PATH" ]
-then
+if [ -d "$TEMP_PATH" ]; then
         cp "$HOME"/.bashrc "$TEMP_PATH"/bashrc.bak
         cp "$HOME"/.bash_aliases "$TEMP_PATH"/bash_aliases.bak
         cp "$HOME"/.vimrc "$TEMP_PATH"/vimrc.bak
         cp "$HOME"/.nanorc "$TEMP_PATH"/nanorc.bak
         cp "$HOME"/.gitconfig "$TEMP_PATH"/gitconfig.bak
         cp "$HOME"/.tmux.conf "$TEMP_PATH"/tmux.conf.bak
+
+        echo "Files Copied" >>"$LOG_FILE"
 fi
 
-if [ -f "$ARCHIVE_PATHFILE" ]
-then
+if [ -f "$ARCHIVE_PATHFILE" ]; then
         rm "$ARCHIVE_PATHFILE"
-        echo "old tar rmvd"
+        echo "$ARCHIVE_PATHFILE removed" >>"$LOG_FILE"
 fi
 
 cd "$TEMP_PATH"
 tar -cf "$ARCHIVE_PATHFILE" .
 
+if [ -f "$ARCHIVE_PATHFILE" ]; then
+        echo "tar success" >>"$LOG_FILE"
+fi
