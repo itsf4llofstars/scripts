4119e7435de5358ee01e1a8791d14ee1e8b90be0
itsf4llofstars <irooted4hal@mailfence.com>
2022-09-17 23:54:53 -0500

    Add echo around chmod commnd
    -c command now has a blank line around it for
    better readability

--- bash/mod.sh
+++ bash/mod.sh
@@ -36,7 +36,9 @@ fi
 
 # checks if the filename path exists, changes permissions if it does
 if [ -f "$1" ]; then
+    echo
     chmod -c 755 "$1"
+    echo
     echo "$(date) File: $1 permissions to 755" >>"$HOME"/logfiles/null.txt
 elif [ ! -f "$1" ]; then
     echo "The file: $1 was not found."
